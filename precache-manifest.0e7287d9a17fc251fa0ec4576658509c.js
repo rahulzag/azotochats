424c35e6173bc6ebafcc1574fc4d8ceaa5a5c35e
rahulzag <rahulzag23@gmail.com>
2020-09-28 13:28:42 +0530

    Updates

---
+++ precache-manifest.0e7287d9a17fc251fa0ec4576658509c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66cc257e82320fa56c8fd37c7a77015",
+    "url": "/azotochat/index.html"
+  },
+  {
+    "revision": "377f9c56a3bbff0a4a2a",
+    "url": "/azotochat/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "6a52d4922012ba3641c9",
+    "url": "/azotochat/static/js/2.f5ab86af.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/azotochat/static/js/2.f5ab86af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "377f9c56a3bbff0a4a2a",
+    "url": "/azotochat/static/js/main.b3714f90.chunk.js"
+  },
+  {
+    "revision": "ceb7ae9457377f6036d9",
+    "url": "/azotochat/static/js/runtime-main.b010f965.js"
+  }
+]);
