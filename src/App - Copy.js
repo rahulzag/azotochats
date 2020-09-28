8e2215b68f963d20e119a90d8c22900b5810f8a3
rahulzag <rahulzag23@gmail.com>
2020-09-28 12:08:19 +0530

    deploy my react app to github pages

---
+++ src/App - Copy.js
@@ -0,0 +1,133 @@
+import React, { useRef, useState } from 'react';
+import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+   apiKey: "AIzaSyASrNKgBo4RPhy8cca-2GLtYtYeZ6u33fA",
+  authDomain: "react-chat-app-6692a.firebaseapp.com",
+  databaseURL: "https://react-chat-app-6692a.firebaseio.com",
+  projectId: "react-chat-app-6692a",
+  storageBucket: "react-chat-app-6692a.appspot.com",
+  messagingSenderId: "92318263915",
+  appId: "1:92318263915:web:3a92382bee9fad42a3df2e",
+  measurementId: "G-Y6W9BQG5LV"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+// eslint-disable-next-line
+const analytics = firebase.analytics();
+
+
+function App() {
+
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Azoto Chat</h1>
+        <SignOut />
+      </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+
+    </div>
+  );
+}
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (<>
+    <main>
+
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+      // eslint-disable-next-line  
+      <button type="submit" disabled={!formValue}>🕊️</button>
+
+    </form>
+  </>)
+}
+
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
+    <div className={`message ${messageClass}`}>
+       // eslint-disable-next-line
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      // eslint-disable-next-line
+       <p>{text}</p>
+    </div>
+  </>)
+}
+
+
+export default App;
